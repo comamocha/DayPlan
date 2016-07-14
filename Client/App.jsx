68e2002c3bfa33351944991da4a8acd68521f140
Hack Reactor Students <accounts@hackreactor.com>
2016-07-14 12:48:19 -0700

    Fix directory structure

---
+++ Client/App.jsx
@@ -0,0 +1,52 @@
+// This is written in es6 syntax
+import React from 'react';
+
+// 
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {username: '', password: ''}
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+/*
+    ***************************************************
+     * @handleUsernameChange
+     * Uses component state to update username on change
+     * e = represents the event.
+    *****************************************************
+*/
+    handleUsernameChange(e) {
+      this.setState({username: e.target.value});
+      console.log(this.state.username);
+    }
+
+    handlePasswordChange(e) {
+      this.setState({password: e.target.value});
+      console.log(this.state.password);
+    }
+
+    handleSubmit(e) {
+      console.log('username: ', this.state.username, '\npassword: ', this.state.password);
+    }
+   render() {
+      return (
+         <div>
+         <img src='http://2.bp.blogspot.com/_VZZE7p2GUjs/SEnKL9DHBxI/AAAAAAAAAN0/FMhKkKzrBN0/s400/Meme' />
+          <nav className='navigation'></nav>
+            <form>
+              <input className='login' placeholder='Enter your username here' onChange={this.handleUsernameChange}/>
+              <input className='password' placeholder='Enter your password here' onChange={this.handlePasswordChange}/>
+              <button className='signIn' type='button' onClick={this.handleSubmit}>Sign In</button>
+              <button className='signUp'>Create a new account</button>
+              <div>{this.props.username}</div>
+            </form>
+         </div>
+      );
+   }
+}
+
+export default App;
---
+++ Client/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+
+// var style = {
+//   background-image = 'http://2.bp.blogspot.com/_VZZE7p2GUjs/SEnKL9DHBxI/AAAAAAAAAN0/FMhKkKzrBN0/s400/Meme'
+// }
+
+ReactDOM.render(<App username='Johnny' password='Johnny'/>, document.getElementById('app'));
