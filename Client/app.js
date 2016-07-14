dbaddcdc1b783ea84e17eba4587c785af63b0a90
Hack Reactor Students <accounts@hackreactor.com>
2016-07-14 14:20:50 -0700

    Prettified login page

--- Client/app.js
+++ Client/app.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 
-// var style = {
-//   background-image = 'http://2.bp.blogspot.com/_VZZE7p2GUjs/SEnKL9DHBxI/AAAAAAAAAN0/FMhKkKzrBN0/s400/Meme'
-// }
-
-ReactDOM.render(<App username='Johnny' password='Johnny'/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
