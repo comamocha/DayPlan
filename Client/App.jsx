795b634394222c10cf3b5008085424ed6dd35589
abraham <abraham.paniagua@gmail.com>
2016-07-15 09:30:22 -0700

    Sample React Routing

--- Client/App.jsx
+++ Client/App.jsx
@@ -1,52 +1,30 @@
 // This is written in es6 syntax
 import React from 'react';
+import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 
-// 
+
+
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {username: '', password: ''}
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    
+  }
 
-/*
-    ***************************************************
-     * @handleUsernameChange
-     * Uses component state to update username on change
-     * e = represents the event.
-    *****************************************************
-*/
-    handleUsernameChange(e) {
-      this.setState({username: e.target.value});
-      console.log(this.state.username);
-    }
 
-    handlePasswordChange(e) {
-      this.setState({password: e.target.value});
-      console.log(this.state.password);
-    }
+  render() {
+    return (
 
-    handleSubmit(e) {
-      console.log('username: ', this.state.username, '\npassword: ', this.state.password);
-    }
-   render() {
-      return (
-         <div className='container'>
-          <nav className='navigation'></nav>
-            <form>
-              <input className='login' placeholder='Username' onChange={this.handleUsernameChange}/>
-              <input className='password' type="password" placeholder='Password' onChange={this.handlePasswordChange}/>
-              <div></div>
-              <button className='signIn' type='button' onClick={this.handleSubmit}>Sign In</button>
-              <button className='signUp'>Create a new account</button>
-              <div>{this.props.username}</div>
-            </form>
-         </div>
-      );
-   }
+      <div>
+        <Link to='/home'>Home</Link>&nbsp;
+        <Link to='/'>App</Link>&nbsp;
+        <Link to='/Login'>Login</Link>&nbsp;
+        {this.props.children}
+      </div>
+
+
+    );
+  }
 }
 
 export default App;
---
+++ Client/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Main extends React.Component {
+   
+    render() {
+        return (
+            <div>
+                Test Main website after login. 
+            </div>
+        );
+    }
+}
+
+export default Main;
+
