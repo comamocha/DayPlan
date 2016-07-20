94aa197f939df90c9ea23db9e01748592e8ed3d1
abraham <abraham.paniagua@gmail.com>
2016-07-20 02:58:16 -0700

    Working Yelp search

--- Client/Components/YelpComponent.jsx
+++ Client/Components/YelpComponent.jsx
@@ -1,16 +1,55 @@
 import React from 'react';
 
+
+//Yelp Component renders yelp search form and results
 class YelpComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {location:'San Francisco', results: 'Searching..' }
-
+    //state location= city you are searchin in
+    //state results = results that returned from Yelp API (Made by us)
+    this.state = { location: 'San Francisco', results: '',solutionObjects:'', currentOverlay:'' }
+    //this.yelpReview.bind(this);
+    
   }
 
-  handleTermChange(term) {
-    console.log(term);
-  }
 
+  //Renders review for business 
+  //ReviewsYelp -> 3 pics, 3 reviews, 
+  // /ReviewsYelp?search=hunan-homes-restaurant-san-francisco
+  yelpReview(businessesId) {
+    self = this;
+    //console.log(businessesId);
+
+    var data = null;
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+        var businessesArray = JSON.parse(this.responseText).reviews;
+        console.log(businessesArray);
+        self.setState({
+          currentOverlay:
+          <div>
+          <h3>{businessesId} </h3>
+          {businessesArray[0].user.name} says {businessesArray[0].text}
+          about 
+          
+          </div>
+        })
+      }
+    })
+
+    xhr.open('GET', 'http://localhost:3000/ReviewsYelp?search=' + businessesId);
+    xhr.send(data);
+    //console.log("clicked that guy",businessesId );
+
+  };
+
+
+  //Searches Yelp API and sets response in State
   onInputChange(term) {
     self = this;
     console.log(term);
@@ -24,26 +63,56 @@ class YelpComponent extends React.Component {
       if (this.readyState === 4) {
         console.log(this.responseText);
 
-        self.setState({ results: this.responseText });
+        //Todo: handle response codes from server
+        //304 = Not Modified (cached version), 200 =	OK, 404 = Not Found 
+        if (JSON.parse(this.responseText).total > 0) {
+          var businessesArray = JSON.parse(this.responseText).businesses;
+
+          //Iterates through the business Array results.
+          //Sample returned Object:
+          //{"rating": 4.0, "image_url": "..", "name": "..", "phone": "", "url": "..", 
+          //"review_count": 2, "coordinates": {"latitude": 33, "longitude": 22}, "id": "..", 
+          //"categories": [{"alias": "theater", "title": "Performing Arts"}], "location": {"city": 
+          //"Daly City", "country": "US", "address2": "", "address3": "", "state": "CA", "address1": 
+          //"", "zip_code": "94016"}}
+
+          var solution = [];
+          var solutionObjects = [];
+
+          for (var i = 0; i < businessesArray.length & i < 3; i++) {
+            solutionObjects.push(businessesArray[i]);
+            solution.push(
+              <tr  placeholder={businessesArray[i].id}  onClick={self.yelpReview.bind(self, businessesArray[i].id)}>
+                <td> {businessesArray[i].name}  </td>
+                <td> {businessesArray[i].location.city} , {businessesArray[i].location.state} </td>
+                <td> {businessesArray[i].location.address1} </td>
+                <td> {businessesArray[i].rating} </td>
+              </tr>
+            );
+
+          }
+
+          self.setState({
+            results: <tbody> {solution} </tbody>
+
+          });
+
+        } else {
+          self.setState({ results: <div> No results </div> });
+        }
 
       }
     });
 
-    xhr.open('GET', 'http://localhost:3000/AutocompleteYelp?search=' + term);
+    xhr.open('GET', 'http://localhost:3000/BusinessYelp?term=' + term + '&location=' + this.state.location);
     xhr.send(data);
-
   }
 
 
   onLocationChange(term) {
     self = this;
     console.log(term);
-     this.setState({ location: term });
-
-  }
-
-  handleSearch(term) {
-   console.log("Searching")
+    this.setState({ location: term });
   }
 
 
@@ -52,37 +121,45 @@ class YelpComponent extends React.Component {
 
     return (
       <div>
-        *YELP*
-        this is the yelp component; talks with yelp API to handle searches
-        i.e.https://github.com/olalonde/node-yelp for yelp search examples
-        location and type (term)
-        build out component to contain time and detail field;
-        should be able to relay information over to our itinerary
 
-
-        <div className='container'>
-
-
+        <div className="text-center" className="col-xs-12 col-xs-offset-1">
+         
           <form>
-
-          <div className="location">
+            <div className="location">
               <input placeholder='San Francisco' onChange={event => this.onLocationChange(event.target.value) } />
-              {this.state.location}
             </div>
-
             <div className="search">
-              <input onChange={event => this.onInputChange(event.target.value) } />
-              {this.state.results}
+              <input placeholder='Golden Gate Bridge' onChange={event => this.onInputChange(event.target.value) } />
             </div>
+            
+            <table className="table table-striped table-hover">
+              <thead className="tableHeader">
+                <tr className="tableHeader">
+                  <th>Name </th>
+                  <th>Location</th>
+                  <th>Address</th>
+                  <th>Rating</th>
+                   
+                </tr>
+              </thead>
+              {this.state.results}
 
-          <button className='searchButton' type='button' onClick={this.handleSearch}>Search</button>
+            </table>
 
 
           </form>
 
+          
+          {this.state.currentOverlay}
+
+          
+           
         </div>
 
+      
 
+
+ 
       </div>
 
     );
