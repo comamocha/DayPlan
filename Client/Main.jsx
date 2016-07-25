1418cc7f91693c1f3d4d4095d44448666487e53a
Conor Carey <careyc@union.edu>
2016-07-25 16:44:29 -0700

    database refactoring + styles

--- Client/Main.jsx
+++ Client/Main.jsx
@@ -157,7 +157,7 @@ class Main extends React.Component {
         <div className="col-xs-12 col-md-5" id="map">
           <MapComponent list={this.state.list}/>
         </div>
-        <div className="col-xs-12 col-md-5 col-md-offset-1" id="itineraryList">
+        <div className="col-xs-12 col-md-6 col-md-offset-1" id="itineraryList">
           <ItineraryListComponent updateActivities={this.updateItineraryActivities.bind(this)}/>
         </div>
 
